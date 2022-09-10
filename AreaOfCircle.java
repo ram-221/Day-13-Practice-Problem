a05abd637480967d4199ac3842763147a2560ab6
ram-221 <rameshmalli857.com>
2022-09-10 12:53:54 +0530

    Program for calculating the Area of a circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args)
+	{
+			
+		double radius = 7;		
+		double area = 3.14 * radius * radius;		
+		System.out.println(area);
+		
+	}
+}
